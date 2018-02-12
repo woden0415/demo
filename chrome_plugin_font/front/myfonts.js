2d8029cf1c98236e2d9d529e5461717afd162da8
woden0415 <woden0415@163.com>
2018-02-12 14:23:30 +0800

    chrome插件

---
+++ chrome_plugin_font/front/myfonts.js
@@ -0,0 +1,4 @@
+window.onload = function () {
+  var oBody = document.querySelector("body");
+  oBody.setAttribute('style', 'font-family: "思源黑体 Regular" !important');
+}
---
+++ chrome_plugin_font/popup.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+  var $btn = document.querySelector(".btn-changeFont");
+  $btn.addEventListener("click", function () {
+    alert(111)
+  }, false);
+}
