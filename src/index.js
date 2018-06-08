955091a50ef0d68dadcfd799ab3a84f5b324342d
CN1\wang_donga <wang_donga@Ctrip.com>
2018-06-08 15:53:26 +0800

    update hock2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 var asdfa = '1';
 function add(a, b) {
-  return a + b
+  return a + b;
 }
 console.log(add('1', 'asdf'));
 function foo(a, b) {
-  console.log("value of the second a:", a);
+  console.log('value of the second a:', a);
 }
 
 var bar = function (a, b) {
-  console.log("value of the second a:", a);
+  console.log('value of the second a:', a);
 };
